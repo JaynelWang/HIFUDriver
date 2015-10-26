5ce690cff370adc52d44b93983a8ec0033abb11d
WANG Shoubei <jaynelwang@163.com>
2015-10-26 13:05:25 +0800

    Revert "clear"
    This reverts commit efa8df1d603d258a9ae0c74e68440b8616c5faf8.

---
+++ lib/common/variable.h
@@ -0,0 +1,52 @@
+#ifndef VARIABLE
+#define VARIABLE
+
+#include "macro.h"
+
+enum cmdType
+{
+    START = 1,
+    STOP,
+    PAUSE,
+    RESUME
+};
+
+typedef struct Plane2DCoordinate
+{
+    Coordinate x;
+    Coordinate y;
+}_2DCor;
+
+typedef struct Spot3DCoordinate
+{
+    Coordinate x;
+    Coordinate y;
+    Coordinate z;
+}_3DCor;
+
+typedef struct SpotSonicationParameter
+{
+    VOLT volt;
+    int totalTime;
+    int period;
+    int dutyCycle;
+    int coolingTime;
+}_SoniParam;
+
+typedef struct SessionRecorder
+{
+    int spotIndex;
+    int periodIndex;
+}_SesRec;
+
+typedef struct SessionParameter
+{
+    int spotCount;
+    int periodCount;
+    int dutyOn;
+    int dutyOff;
+    int coolingTime;
+}_SesParam;
+
+#endif // VARIABLE
+
